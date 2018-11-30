4f4d647f1ad37a7b84e7eaddc09d78ef81d4f625
Boris Bosiljcic <boris.bosiljcic@presono.com>
2018-11-30 12:35:48 +0100

    improve express to work better with react

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const logger = require('morgan');
 
 const port = process.env.PORT || 5000;
 
-const indexRouter = require('./routes/index');
 const loginRouter = require('./routes/login');
 const forumsRouter = require('./routes/forums');
 const postsRouter = require('./routes/posts');
 const usersRouter = require('./routes/users');
 
-
 const app = express();
+
+const apiPrefix = '/api';
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,15 +20,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/login', loginRouter);
-app.use('/forums', forumsRouter);
-app.use('/posts', postsRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+app.use(`${apiPrefix}/login`, loginRouter);
+app.use(`${apiPrefix}/forums`, forumsRouter);
+app.use(`${apiPrefix}/posts`, postsRouter);
+app.use(`${apiPrefix}/users`, usersRouter);
 
-app.get('/backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+app.get(`${apiPrefix}/test`, (req, res) => {
+  res.send({ express: 'backend connected' });
 });
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
