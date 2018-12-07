b6e34ea8d0b5e8dcd859d57f44c807fa68c68273
Boris Bosiljcic <boris.bosiljcic@presono.com>
2018-12-07 14:38:10 +0100

    add pagination to topics, improve post display

--- client/src/components/TopicPage.js
+++ client/src/components/TopicPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactPaginate from 'react-paginate';
 import { getTopicById } from '../services/api';
 import Post from './topicPage/Post';
 
@@ -11,6 +12,8 @@ export default class TopicPage extends Component {
       posts: [],
       id: match.params.id || 154,
       page: match.params.page || 1,
+      pageCount: null,
+
     };
   }
 
@@ -21,7 +24,16 @@ export default class TopicPage extends Component {
   async loadForum() {
     const { id, page } = this.state;
     const response = await getTopicById(id, page);
-    this.setState({ posts: response.data.topics });
+    this.setState({ posts: response.data.topics, pageCount: response.data.pages });
+  }
+
+  async handlePageClick(data) {
+    const { id } = this.state;
+    const page = data.selected;
+    const response = await getTopicById(id, page);
+    this.setState({ posts: response.data.topics, page, pageCount: response.data.pages }, () => {
+      window.scrollTo(0, 0);
+    });
   }
 
 
@@ -31,9 +43,28 @@ export default class TopicPage extends Component {
   }
 
   render() {
+    const { pageCount } = this.state;
+
+    const pagination = (
+      <div className="pagination">
+        <ReactPaginate
+          previousLabel="<"
+          nextLabel=">"
+          breakLabel="..."
+          pageCount={pageCount}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={3}
+          onPageChange={this.handlePageClick.bind(this)}
+          activeClassName="active"
+        />
+      </div>
+    );
+
     return (
       <div>
+        {pagination}
         {this.renderPosts()}
+        {pagination}
       </div>
     );
   }
